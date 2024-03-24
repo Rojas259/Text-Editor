397386d43544dbf2af4769839b8e11bfb302448b
Rojas259 <147790485+Rojas259@users.noreply.github.com>
2024-03-23 21:04:53 -0700

    update to Javascript

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  console.log('putDb not implemented');
+  const contactDb = await openDB('jate', 1);
+  const transaction = contactDb.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('getDb not implemented');
+  const contactDb = await openDB('jate', 1);
+  const transaction = contactDb.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('🚀 - data retrieved from the database', result)
+  return result?.value;
+}
+
 
 initdb();
